f85f913c2a93e6dc046d1c3ef825a1a8bdc32c1d
kokodev <github@kokofixcomputers.serv00.net>
2025-02-11 10:15:49 -0800

    Update overview.tsx

--- app/routes/settings/overview.tsx
+++ app/routes/settings/overview.tsx
@@ -3,6 +3,7 @@ import { Link as RemixLink } from 'react-router';
 import Button from '~/components/Button';
 import Link from '~/components/Link';
 import cn from '~/utils/cn';
+import Card from '~/components/Card';
 
 import AgentSection from './components/agent';
 
@@ -17,9 +18,11 @@ export default function Page() {
 					free to open an issue on the GitHub repository.
 				</p>
 			</div>
-			<div className="flex flex-col w-2/3">
-				<h1 className="text-2xl font-medium mb-4">Pre-Auth Keys</h1>
-				<p>
+			<Card variant="flat" className="max-w-prose mt-12">
+				<div className="flex items-center justify-between">
+					<Card.Title className="text-xl mb-0">Pre-Auth Keys</Card.Title>
+				</div>
+				<Card.Text className="mt-4">
 					Headscale fully supports pre-authentication keys in order to easily
 					add devices to your Tailnet. To learn more about using
 					pre-authentication keys, visit the{' '}
@@ -29,14 +32,15 @@ export default function Page() {
 					>
 						Tailscale documentation
 					</Link>
-				</p>
-			</div>
-			<RemixLink to="/settings/auth-keys">
-				<div className="text-lg font-medium flex items-center">
-					Manage Auth Keys
-					<ArrowRightIcon className="w-5 h-5 ml-2" />
-				</div>
-			</RemixLink>
+					
+					<RemixLink to="/settings/auth-keys">
+						<div className="text-lg font-medium flex items-center">
+							Manage Auth Keys
+							<ArrowRightIcon className="w-5 h-5 ml-2" />
+						</div>
+					</RemixLink>
+				</Card.Text>
+			</Card>
 			{/**<AgentSection />**/}
 		</div>
 	);
