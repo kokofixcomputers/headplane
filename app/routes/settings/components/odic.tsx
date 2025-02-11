8f613d099a9199537957c5db77c7dfad85683e4e
kokofixcomputers <github@kokofixcomputers.serv00.net>
2025-02-11 11:00:28 -0800

    Add ODIC Settings

---
+++ app/routes/settings/components/odic.tsx
@@ -0,0 +1,104 @@
+import { useState } from 'react';
+import { useFetcher } from 'react-router';
+
+import Dialog from '~/components/Dialog';
+import Input from '~/components/Input';
+import Spinner from '~/components/Spinner';
+
+type Properties = {
+	readonly issuer: string;
+	readonly disabled?: boolean;
+    readonly client_id: string;
+    readonly client_secret: string;
+};
+
+// TODO: Switch to form submit instead of JSON patch
+export default function Modal({ issuer, client_id, client_secret, disabled }: Properties) {
+	const [newIssuer, setNewIssuer] = useState(issuer);
+    const [newClient_id, setNewClient_id] = useState(client_id);
+    const [newClient_secret, setNewClient_secret] = useState(client_secret);
+	const fetcher = useFetcher();
+
+	return (
+		<div className="flex flex-col w-2/3 gap-y-4">
+			<h1 className="text-2xl font-medium mb-2">Tailnet Name</h1>
+			<p>
+				Configure ODIC Authentication Settings
+			</p>
+			<Input
+                isReadOnly
+				className="w-3/5 font-medium text-sm"
+				label="Issuer"
+				value={issuer}
+				onFocus={(event) => {
+					event.target.select();
+				}}
+			/>
+            <Input
+                isReadOnly
+				className="w-3/5 font-medium text-sm"
+				label="Client ID"
+				value={client_id}
+				onFocus={(event) => {
+					event.target.select();
+				}}
+			/>
+            <Input
+                isReadOnly
+				className="w-3/5 font-medium text-sm"
+				label="Client Secret"
+                type="password"
+				value={client_secret}
+				onFocus={(event) => {
+					event.target.select();
+				}}
+			/>
+
+			<Dialog>
+				<Dialog.Button isDisabled={disabled}>
+					{fetcher.state === 'idle' ? undefined : (
+						<Spinner className="w-3 h-3" />
+					)}
+					Save
+				</Dialog.Button>
+				<Dialog.Panel
+					onSubmit={() => {
+						fetcher.submit(
+							{
+								'oidc.issuer': newIssuer,
+                                'oidc.client_id': newClient_id,
+                                'oidc.client_secret': newClient_secret,
+							},
+							{
+								method: 'PATCH',
+								encType: 'application/json',
+							},
+						);
+					}}
+				>
+					<Dialog.Title>Change OCID Information</Dialog.Title>
+					<Dialog.Text>
+						Keep in mind that changing this can break ODIC authentication if configured improperly.
+					</Dialog.Text>
+					<Input
+						label="Issuer"
+                        value={issuer}
+						placeholder="ts.net"
+						onChange={setNewIssuer}
+					/>
+                    <Input
+						label="Client ID"
+                        value={client_id}
+						placeholder="ts.net"
+						onChange={setNewClient_id}
+					/>
+                    <Input
+						label="Client Secret" // Value won't be added for privacy reasons
+						placeholder="ts.net"
+						onChange={setNewClient_secret}
+					/>
+				</Dialog.Panel>
+			</Dialog>
+		</div>
+	);
+}
--- app/routes/settings/overview.tsx
+++ app/routes/settings/overview.tsx
@@ -1,15 +1,71 @@
+import type { ActionFunctionArgs } from 'react-router';
+import { data, useLoaderData } from 'react-router';
 import { ArrowRightIcon } from '@primer/octicons-react';
 import { Link as RemixLink } from 'react-router';
-import Button from '~/components/Button';
 import Link from '~/components/Link';
-import cn from '~/utils/cn';
 import Card from '~/components/Card';
-
+import Notice from '~/components/Notice';
+import { loadContext } from '~/utils/config/headplane';
+import { loadConfig, patchConfig } from '~/utils/config/headscale';
+import { getSession } from '~/utils/sessions.server';
+import OdicModal from './components/odic';
 import AgentSection from './components/agent';
 
+export async function loader() {
+	const context = await loadContext();
+	if (!context.config.read) {
+		throw new Error('No configuration is available');
+	}
+
+	const config = await loadConfig();
+	const odicdata = config.oidc ? {
+		issuer: config.oidc.issuer,
+		client_id: config.oidc.client_id,
+		client_secret: config.oidc.client_secret,
+	} : {};
+
+	return {
+		...odicdata,
+		...context,
+	};
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+	const session = await getSession(request.headers.get('Cookie'));
+	if (!session.has('hsApiKey')) {
+		return data({ success: false }, { status: 401 });
+	}
+
+	const context = await loadContext();
+	if (!context.config.write) {
+		return data({ success: false }, { status: 403 });
+	}
+
+	const textData = await request.text();
+	if (!textData) {
+		return data({ success: true });
+	}
+
+	const patch = JSON.parse(textData) as Record<string, unknown>;
+	await patchConfig(patch);
+
+	if (context.integration?.onConfigChange) {
+		await context.integration.onConfigChange(context.integration.context);
+	}
+
+	return data({ success: true });
+}
+
 export default function Page() {
+	const data = useLoaderData<typeof loader>();
 	return (
 		<div className="flex flex-col gap-8 max-w-screen-lg">
+			{data.config.write ? undefined : (
+				<Notice>
+					The Headscale configuration is read-only. You cannot make changes to
+					the configuration
+				</Notice>
+			)}
 			<div className="flex flex-col w-2/3">
 				<h1 className="text-2xl font-medium mb-4">Settings</h1>
 				<p>
@@ -41,6 +97,8 @@ export default function Page() {
 					</RemixLink>
 				</Card.Text>
 			</Card>
+
+			<OdicModal issuer={data.issuer ?? ''} client_id={data.client_id ?? ''} client_secret={data.client_secret ?? ''} disabled={!data.config.write}></OdicModal>
 			{/**<AgentSection />**/}
 		</div>
 	);
