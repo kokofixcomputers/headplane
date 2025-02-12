ef7a5d2475f51b7192f6ab761d79b745ac4a95e9
kokofixcomputers <github@kokofixcomputers.serv00.net>
2025-02-11 20:28:44 -0800

    Move settings around.

--- app/routes/settings/components/oidc.tsx
+++ app/routes/settings/components/oidc.tsx
@@ -5,6 +5,7 @@ import Dialog from '~/components/Dialog';
 import Input from '~/components/Input';
 import Spinner from '~/components/Spinner';
 import OidcDomainModal from '../components/oidcAllowedDomains';
+import Card from '~/components/Card';
 
 type Properties = {
 	readonly issuer: string;
@@ -44,93 +45,97 @@ export default function Modal({ issuer, client_id, client_secret, disabled, only
 
 	return (
 		<div className="flex flex-col w-2/3 gap-y-4">
-			<h1 className="text-2xl font-medium mb-2">OIDC Settings</h1>
-			<p>
-				Configure OIDC Authentication Settings
-			</p>
-			<OIDCSwitch />
-			<Input
-                isReadOnly
-				className="w-3/5 font-medium text-sm"
-				label="Issuer"
-				value={issuer}
-				onFocus={(event) => {
-					event.target.select();
-				}}
-			/>
-            <Input
-                isReadOnly
-				className="w-3/5 font-medium text-sm"
-				label="Client ID"
-				value={client_id}
-				onFocus={(event) => {
-					event.target.select();
-				}}
-			/>
-            <Input
-                isReadOnly
-				className="w-3/5 font-medium text-sm"
-				label="Client Secret"
-                type="password"
-				value={client_secret}
-				onFocus={(event) => {
-					event.target.select();
-				}}
-			/>
-
-			<Dialog>
-				<Dialog.Button isDisabled={disabled}>
-					{fetcher.state === 'idle' ? undefined : (
-						<Spinner className="w-3 h-3" />
-					)}
-					Change ODIC Settings
-				</Dialog.Button>
-				<Dialog.Panel
-					onSubmit={() => {
-						fetcher.submit(
-							{
-								'oidc.only_start_if_oidc_is_available': newOnly_start_if_oidc_is_available,
-								'oidc.issuer': newIssuer,
-                                'oidc.client_id': newClient_id,
-                                'oidc.client_secret': newClient_secret,
-							},
-							{
-								method: 'PATCH',
-								encType: 'application/json',
-							},
-						);
-					}}
-				>
-					<Dialog.Title>Change OCID Information</Dialog.Title>
-					<Dialog.Text>
-						Keep in mind that changing this can break ODIC authentication if configured improperly.
-					</Dialog.Text>
-					<p>Only Start HeadScale if OIDC Authentication is available</p>
-					<Switch
-						label="Only Start HeadScale if OIDC Authentication is available"
-						defaultSelected={only_start_if_oidc_is_available}
-						onChange={setOnly_start_if_oidc_is_available}
+			<Card variant="flat" className="max-w-prose mt-12">
+				<div className="flex items-center justify-between">
+					<Card.Title className="text-xl mb-0">OIDC Settings</Card.Title>
+				</div>
+				<Card.Text className="mt-4">
+					Configure OIDC Authentication Settings
+					<OIDCSwitch />
+					<Input
+						isReadOnly
+						className="w-3/5 font-medium text-sm"
+						label="Issuer"
+						value={issuer}
+						onFocus={(event) => {
+							event.target.select();
+						}}
 					/>
 					<Input
-						label="Issuer"
-                        value={issuer}
-						placeholder="Your Issuer"
-						onChange={setNewIssuer}
-					/>
-                    <Input
+						isReadOnly
+						className="w-3/5 font-medium text-sm"
 						label="Client ID"
-                        value={client_id}
-						placeholder="Your client ID"
-						onChange={setNewClient_id}
+						value={client_id}
+						onFocus={(event) => {
+							event.target.select();
+						}}
 					/>
-                    <Input
-						label="Client Secret" // Value won't be added for privacy reasons
-						placeholder="Your client secret"
-						onChange={setNewClient_secret}
+					<Input
+						isReadOnly
+						className="w-3/5 font-medium text-sm"
+						label="Client Secret"
+						type="password"
+						value={client_secret}
+						onFocus={(event) => {
+							event.target.select();
+						}}
 					/>
-				</Dialog.Panel>
-			</Dialog>
-			<OidcDomainModal allowed_domains={allowed_domains ?? []} disabled={disabled} />		
+
+					<Dialog>
+						<Dialog.Button isDisabled={disabled}>
+							{fetcher.state === 'idle' ? undefined : (
+								<Spinner className="w-3 h-3" />
+							)}
+							Change ODIC Settings
+						</Dialog.Button>
+						<Dialog.Panel
+							onSubmit={() => {
+								fetcher.submit(
+									{
+										'oidc.only_start_if_oidc_is_available': newOnly_start_if_oidc_is_available,
+										'oidc.issuer': newIssuer,
+										'oidc.client_id': newClient_id,
+										'oidc.client_secret': newClient_secret,
+									},
+									{
+										method: 'PATCH',
+										encType: 'application/json',
+									},
+								);
+							}}
+						>
+							<Dialog.Title>Change OCID Information</Dialog.Title>
+							<Dialog.Text>
+								Keep in mind that changing this can break ODIC authentication if configured improperly.
+							</Dialog.Text>
+							<p>Only Start HeadScale if OIDC Authentication is available</p>
+							<Switch
+								label="Only Start HeadScale if OIDC Authentication is available"
+								defaultSelected={only_start_if_oidc_is_available}
+								onChange={setOnly_start_if_oidc_is_available}
+							/>
+							<Input
+								label="Issuer"
+								value={issuer}
+								placeholder="Your Issuer"
+								onChange={setNewIssuer}
+							/>
+							<Input
+								label="Client ID"
+								value={client_id}
+								placeholder="Your client ID"
+								onChange={setNewClient_id}
+							/>
+							<Input
+								label="Client Secret" // Value won't be added for privacy reasons
+								placeholder="Your client secret"
+								onChange={setNewClient_secret}
+							/>
+						</Dialog.Panel>
+					</Dialog>
+					<OidcDomainModal allowed_domains={allowed_domains ?? []} disabled={disabled} />		
+				</Card.Text>
+			</Card>
 		</div>	
 	);
 }
--- app/routes/settings/components/oidcAllowedDomains.tsx
+++ app/routes/settings/components/oidcAllowedDomains.tsx
@@ -5,6 +5,7 @@ import cn from '~/utils/cn';
 import Dialog from '~/components/Dialog';
 import Spinner from '~/components/Spinner';
 import AddAllowedDomain from '../dialogs/allowedDomains';
+import Card from '~/components/Card';
 
 type Properties = {
 	readonly disabled?: boolean;
@@ -18,88 +19,92 @@ export default function Modal({ disabled, allowed_domains }: Properties) {
 
 	return (
 		<div className="flex flex-col w-2/3 gap-y-4">
-			<h1 className="text-2xl font-medium mb-2">OIDC Domain Settings</h1>
-			<p>
-				Configure OIDC Domain Settings
-			</p>
-			<div className="mt-4">
-				<h2 className="text-2xl font-medium mb-4">Allowed Domains</h2>
-				<TableList className="mb-8">
-					{allowed_domains.length === 0 ? (
-						<TableList.Item>
-							<p className="opacity-50 mx-auto">No allowed domain. All will be allowed.</p>
-						</TableList.Item>
-					) : (
-						allowed_domains.map((allowed_domain, index) => (
-							<TableList.Item key={`${allowed_domain}`}>
-								<div className="flex gap-24 items-center">
-									<div className="flex gap-4 items-center">
-										<p className="font-mono text-sm">{allowed_domain}</p>
-									</div>
-								</div>
-							</TableList.Item>
-						))
-					)}
-				</TableList>
-			</div>
+            <Card variant="flat" className="max-w-prose mt-12">
+                <div className="flex items-center justify-between">
+                    <Card.Title className="text-xl mb-0">OIDC Domain Settings</Card.Title>
+                </div>
+                <Card.Text className="mt-4">
+                    Configure OIDC Domain Settings
+                    <div className="mt-4">
+                        <h2 className="text-2xl font-medium mb-4">Allowed Domains</h2>
+                        <TableList className="mb-8">
+                            {allowed_domains.length === 0 ? (
+                                <TableList.Item>
+                                    <p className="opacity-50 mx-auto">No allowed domain. All will be allowed.</p>
+                                </TableList.Item>
+                            ) : (
+                                allowed_domains.map((allowed_domain, index) => (
+                                    <TableList.Item key={`${allowed_domain}`}>
+                                        <div className="flex gap-24 items-center">
+                                            <div className="flex gap-4 items-center">
+                                                <p className="font-mono text-sm">{allowed_domain}</p>
+                                            </div>
+                                        </div>
+                                    </TableList.Item>
+                                ))
+                            )}
+                        </TableList>
+                    </div>
 
-			<Dialog>
-				<Dialog.Button isDisabled={disabled}>
-					{fetcher.state === 'idle' ? undefined : (
-						<Spinner className="w-3 h-3" />
-					)}
-					Change Domains Settings
-				</Dialog.Button>
-				<Dialog.Panel>
-					<Dialog.Title>Change Domains Settings</Dialog.Title>
-					<Dialog.Text>
-						Keep in mind that changing this can break ODIC authentication if configured improperly.
-					</Dialog.Text>
-					<div className="mt-4">
-					<h2 className="text-2xl font-medium mb-4">Allowed Domains</h2>
-					<TableList className="mb-8">
-						{allowed_domains.length === 0 ? (
-							<TableList.Item>
-								<p className="opacity-50 mx-auto">No allowed domain. All will be allowed.</p>
-							</TableList.Item>
-						) : (
-							allowed_domains.map((allowed_domain, index) => (
-								<TableList.Item key={`${allowed_domain}`}>
-									<div className="flex gap-24 items-center">
-										<div className="flex gap-4 items-center">
-											<p className="font-mono text-sm">{allowed_domain}</p>
-										</div>
-									</div>
-									<Button
-										className={cn(
-											'px-2 py-1 rounded-md',
-											'text-red-500 dark:text-red-400',
-										)}
-										isDisabled={disabled}
-										onPress={() => {
-											fetcher.submit(
-												{
-													'oidc.allowed_domains': allowed_domains.filter(
-														(_, i) => i !== index,
-													),
-												},
-												{
-													method: 'PATCH',
-													encType: 'application/json',
-												},
-											);
-										}}
-									>
-										Remove
-									</Button>
-								</TableList.Item>
-							))
-						)}
-					</TableList>
-					{disabled ? undefined : <AddAllowedDomain records={allowed_domains} />}
-				</div>
-				</Dialog.Panel>
-			</Dialog>
+                    <Dialog>
+                        <Dialog.Button isDisabled={disabled}>
+                            {fetcher.state === 'idle' ? undefined : (
+                                <Spinner className="w-3 h-3" />
+                            )}
+                            Change Domains Settings
+                        </Dialog.Button>
+                        <Dialog.Panel>
+                            <Dialog.Title>Change Domains Settings</Dialog.Title>
+                            <Dialog.Text>
+                                Keep in mind that changing this can break ODIC authentication if configured improperly.
+                            </Dialog.Text>
+                            <div className="mt-4">
+                            <h2 className="text-2xl font-medium mb-4">Allowed Domains</h2>
+                            <TableList className="mb-8">
+                                {allowed_domains.length === 0 ? (
+                                    <TableList.Item>
+                                        <p className="opacity-50 mx-auto">No allowed domain. All will be allowed.</p>
+                                    </TableList.Item>
+                                ) : (
+                                    allowed_domains.map((allowed_domain, index) => (
+                                        <TableList.Item key={`${allowed_domain}`}>
+                                            <div className="flex gap-24 items-center">
+                                                <div className="flex gap-4 items-center">
+                                                    <p className="font-mono text-sm">{allowed_domain}</p>
+                                                </div>
+                                            </div>
+                                            <Button
+                                                className={cn(
+                                                    'px-2 py-1 rounded-md',
+                                                    'text-red-500 dark:text-red-400',
+                                                )}
+                                                isDisabled={disabled}
+                                                onPress={() => {
+                                                    fetcher.submit(
+                                                        {
+                                                            'oidc.allowed_domains': allowed_domains.filter(
+                                                                (_, i) => i !== index,
+                                                            ),
+                                                        },
+                                                        {
+                                                            method: 'PATCH',
+                                                            encType: 'application/json',
+                                                        },
+                                                    );
+                                                }}
+                                            >
+                                                Remove
+                                            </Button>
+                                        </TableList.Item>
+                                    ))
+                                )}
+                            </TableList>
+                            {disabled ? undefined : <AddAllowedDomain records={allowed_domains} />}
+                        </div>
+                        </Dialog.Panel>
+                    </Dialog>
+                </Card.Text>
+            </Card>
 		</div>
 	);
 }
