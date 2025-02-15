42f0b36eccd3c75a77334cf5786e8e5aa6a186e4
kokofixcomputers <github@kokofixcomputers.serv00.net>
2025-02-14 16:02:38 -0800

    Use IIFE to handle await

--- app/db/database.ts
+++ app/db/database.ts
@@ -3,17 +3,19 @@ import sqlite3 from 'sqlite3';
 import { loadConfig } from '~/utils/config/headscale';
 import { loadContext } from '~/utils/config/headplane';
 
-const context = await loadContext();
-let filename: string = '/var/lib/headscale/db.sqlite';
+const dbPromise = (async () => {
+  const context = await loadContext();
+  let filename: string = '/var/lib/headscale/db.sqlite';
 
-if (context.config.read) {
-  const config = await loadConfig();
+  if (context.config.read) {
+    const config = await loadConfig();
     filename = config.database.sqlite.path;
-}
+  }
 
-const dbPromise = open({
-  filename: filename,
-  driver: sqlite3.Database,
-});
+  return open({
+    filename: filename,
+    driver: sqlite3.Database,
+  });
+})();
 
 export default dbPromise;
